30138ea556d036e3f2cf9950dfb9938928babfd9
Shawn Halayka <sjhalayka@gmail.com>
2021-08-27 11:42:02 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+using namespace std;
+
+#include "main.h"
+
+
+
+
+
+
+
+int main(void)
+{
+	ofstream out_bin("points.bin", ios_base::binary);
+
+	const size_t res = 300;
+	const float grid_max = 1.5;
+	const float grid_min = -grid_max;
+	const unsigned short int max_iterations = 8;
+	const float threshold = 4.0f;
+	const float step_size = (grid_max - grid_min) / (res - 1);
+
+	threeonion C;
+	C.vertex_data[0] = 0.2f;
+	C.vertex_data[1] = 0.5f;
+	C.vertex_data[2] = 0.3f;
+
+	threeonion Z;
+
+	for (size_t i = 0; i < Z.vertex_length; i++)
+		Z.vertex_data[i] = grid_min;
+
+	size_t total_count = 0;
+	size_t in_set = 0;
+
+	for (size_t i0 = 0; i0 < res; i0++, Z.vertex_data[0] += step_size)
+	{
+		cout << i0 + 1 << " of " << res << endl;
+
+		Z.vertex_data[1] = grid_min;
+
+		for (size_t i1 = 0; i1 < res; i1++, Z.vertex_data[1] += step_size)
+		{
+			Z.vertex_data[2] = grid_min;
+
+			for (size_t i2 = 0; i2 < res; i2++, Z.vertex_data[2] += step_size)
+			{
+				total_count++;
+
+				float magnitude = iterate(Z, C, max_iterations, threshold);
+
+				if (magnitude < threshold)
+				{
+					in_set++;
+
+					out_bin.write(reinterpret_cast<const char*>(&Z.vertex_data[0]), sizeof(float));
+					out_bin.write(reinterpret_cast<const char*>(&Z.vertex_data[1]), sizeof(float));
+					out_bin.write(reinterpret_cast<const char*>(&Z.vertex_data[2]), sizeof(float));
+				}
+			}
+		}
+	}
+
+	out_bin.close();
+
+	cout << in_set << " of " << total_count << endl;
+
+	return 0;
+}
